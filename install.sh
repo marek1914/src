256ef7ad226bc8b55ac981b4a1514010e21ace75
home <13492467@qq.com>
2017-08-13 22:27:20 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# 若不删除，下面的 -f 最造成目录链接的嵌套，奇怪
+rm ~/.vim/vim-snippets/UltiSnips
+rm ~/.vim/doc
+
 ln -sf $PWD/base/snippet/ ~/.vim/vim-snippets/UltiSnips
 ln -sf $PWD/vim/vimrc ~/.vimrc
 ln -sf $PWD/docs/txt/vihelp/ ~/.vim/doc
