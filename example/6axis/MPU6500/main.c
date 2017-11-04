4c76deb1cf4731a8e5334bd096133139d2ed81cc
home <13492467@qq.com>
2017-11-04 16:42:23 +0800

    fix

--- example/6axis/MPU6500/main.c
+++ example/6axis/MPU6500/main.c
@@ -9,20 +9,7 @@
 #include "inv_mpu.h"
 #include "dmpmap.h"
 #include "dmpKey.h"
-
-// inv_mpu_dmp_motion_driver.h
-
-struct hal_s {
-    unsigned char sensors;
-    unsigned char dmp_on;
-    unsigned char wait_for_tap;
-    volatile unsigned char new_gyro;
-    unsigned short report;
-    unsigned short dmp_features;
-    unsigned char motion_int_mode;
-    struct rx_s rx;
-};
-static struct hal_s hal = {0};
+#include "inv_mpu_dmp_motion_driver.h"
 
 
 int i2c_write(uint8_t dev, uint8_t reg, uint8_t len, uint8_t* data) 
@@ -70,6 +57,7 @@ int main(void)
 	short gyro[3], accel[3], sensors;
 	unsigned char more;
 	long quat[4];
+	unsigned long sensor_timestamp;
 
 	unsigned char accel_fsr;
     unsigned short gyro_rate, gyro_fsr;
@@ -108,7 +96,7 @@ int main(void)
         DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_TAP |
         DMP_FEATURE_ANDROID_ORIENT | DMP_FEATURE_SEND_RAW_ACCEL | 
 		DMP_FEATURE_SEND_CAL_GYRO |
-        DMP_FEATURE_GYRO_CAL;);
+        DMP_FEATURE_GYRO_CAL);
     dmp_set_fifo_rate(DEFAULT_MPU_HZ);
 
 	mpu_set_dmp_state(1);
