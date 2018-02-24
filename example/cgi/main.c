865180b4a262494de77c836cceda48b7c9f26768
GaoJie <jackgao@qiyi.com>
2018-02-24 17:02:21 +0800

    fix

---
+++ example/cgi/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main(void)
+{
+	printf("Content-type: text/html\n\n");
+	printf("<html lang=\"en\">\n");
+	printf("<head>\n");
+	printf("<title>XXXXXX</title>\n");
+	printf("<head>\n");
+
+	printf("<body>\n");
+	printf("<p>hello gaojie</p>\n");
+	printf("</body>\n");
+	printf("</html>\n");
+}
